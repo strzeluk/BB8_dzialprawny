20b4d4c572b55b6bfb66dd0cf0087e0b392cabfc
Łukasz Strzelecki <strzeluk@gmail.com>
2020-01-21 18:48:38 +0100

    Revert "Revert "Skończony dashboard w wersji desktop""
    This reverts commit b19cd3f7e4c7332b6adb38d3fab3de701584450b.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
 });
 
 $(function () {
-    // Enables popover
-    $("[data-toggle=popover]").popover();
-});
+    $('[data-toggle="popover"]').popover({
+        html: true,
+        content: function () {
+            return $('#' + this.id + 'Popover').html();
+        }
+    });
+})
